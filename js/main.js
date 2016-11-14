fe76ffd5f3da65c0b4f3fcb658082d9360c39203
Vasile Taranovici <vasile.taranovici@gmail.comgit config --global user.email vasile.taranovici@gmail.comgit config --global user.email vasile.taranovici@gmail.comcleargit config --global user.name Vasile>
2016-11-15 01:02:57 +0200

    changed scroll menu, added some temporary stuff in about section + hover menu, some responsiveness to testimonials on mobile and removed an element from universities + added Vue on top of the page

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@
         $('.button-collapse').sideNav();
         // $('.scrollspy').scrollSpy({scrollOffset:0});
         $('#lightgallery').lightGallery();
-        $('.scrollspy').scrollSpy({scrollOffset:250});
+
+        $('.scrollspy').scrollSpy({scrollOffset: 50});
 
 
         $('.button-collapse').sideNav({
@@ -11,23 +12,5 @@
             edge: 'left',
             closeOnClick: true
         });
-        // $('.parallax').parallax();
-        // var card = document.querySelectorAll('.card-work');
-        // var transEndEventNames = {
-        //         'WebkitTransition': 'webkitTransitionEnd',
-        //         'MozTransition': 'transitionend',
-        //         'transition': 'transitionend'
-        //     },
-        //     transEndEventName = transEndEventNames[Modernizr.prefixed('transition')];
-
     });
-    // $(document).ready(function(){
-    //     $(window).scroll(function(){
-    //         if ($(this).scrollTop() > 450) {
-    //             $('#nav_f').fadeIn(700);
-    //         } else {
-    //             $('#nav_f').fadeOut(700);
-    //         }
-    //     });
-    // });
 })(jQuery);
