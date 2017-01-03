ff31a71cfb834701352f66aa8485ccee1dfb20de
Vasile Taranovici <vasile.taranovici@gmail.com>
2017-01-03 23:33:07 +0200

    modified about/uni, added alpha contest, modified testimonial width

--- js/main.js
+++ js/main.js
@@ -2,9 +2,15 @@
     $(function() {
         $('.button-collapse').sideNav();
         // $('.scrollspy').scrollSpy({scrollOffset:0});
+
+        $('.modal-trigger').leanModal();
+
         $('#lightgallery').lightGallery();
 
         $('.scrollspy').scrollSpy({scrollOffset: 50});
+
+
+
 
 
         $('.button-collapse').sideNav({
