3a129cccd6e3f879e93eff6e1b93bf11d3cd4ce1
Vasile Taranovici <vasile.taranovici@gmail.com>
2017-01-18 15:16:45 +0200

    changed video bg from local not YT, added slider for mobile and tablets, added pause video button, modified contest position, added all the compressed photos to the gallery etc

--- js/main.js
+++ js/main.js
@@ -12,26 +12,91 @@
 
 
 
-
+// menu collapse
         $('.button-collapse').sideNav({
             menuWidth: 240,
             edge: 'left',
             closeOnClick: true
         });
-
+// animation
         new WOW().init();
     });
 })(jQuery);
 
-
+// preloader
 document.addEventListener("DOMContentLoaded", function(){
-	$('.preloader-background').delay(1700).fadeOut('slow');
+	$('.preloader-background').delay(500).fadeOut('slow');
 
 	$('.preloader-wrapper')
-		.delay(1700)
+		.delay(500)
 		.fadeOut();
 });
+
+
 
 $(document).ready(function(){
          $('ul.tabs').tabs();
        });
+
+
+// video
+var vid = document.getElementById("bgvid"),
+pauseButton = document.getElementById("vidpause");
+function vidFade() {
+    vid.classList.add("stopfade");
+}
+vid.addEventListener('ended', function() {
+    // only functional if "loop" is removed
+     vid.pause();
+	// to capture IE10
+	vidFade();
+});
+pauseButton.addEventListener("click", function() {
+    vid.classList.toggle("stopfade");
+	if (vid.paused) {
+vid.play();
+		pauseButton.innerHTML = "II";
+	} else {
+        vid.pause();
+        pauseButton.innerHTML = "►";
+	}
+})
+
+// scroll to top button
+$(document).ready(function() {
+if ($('.mbr-arrow-up').length) {
+    var $scroller = $('#scrollToTop'),
+        $main = $('body,html'),
+        $window = $(window);
+    $scroller.css('display', 'none');
+    $window.scroll(function () {
+    if ($(this).scrollTop() > 0) {
+        $scroller.fadeIn();
+    } else {
+        $scroller.fadeOut();
+    }
+    });
+    $scroller.click(function() {
+        $main.animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    });
+}
+});
+
+// Owl Carousel init
+$(document).ready(function()
+{
+  var carousel = $(".owl-carousel");
+	if(carousel.length > 0)
+	{
+		var data = carousel.data();
+		$(".owl-carousel").owlCarousel(data);
+	}
+});
+
+// slider mtz
+$(document).ready(function(){
+  $('.slider').slider({full_width: true});
+});
--- js/owl.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function()
-{
-  var carousel = $(".owl-carousel");
-	if(carousel.length > 0)
-	{
-		var data = carousel.data();
-		$(".owl-carousel").owlCarousel(data);
-	}
-});
